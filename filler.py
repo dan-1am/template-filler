83d98405b011728f5d6395840249f767976fa9f8
Denis Orekhov <oda16m@gmail.com>
2023-11-06 15:40:16 +0700

    v1.03 comments, __all__

--- filler.py
+++ filler.py
@@ -1,12 +1,35 @@
-# v1.02
+""" Template filling with context
 
-#import re
-#def eval_template(src, context):
-#    return re.sub(r'\{\{(.*?)\}\}', lambda m: str( eval(m[1], context) ), src)
+Simplest usage:
+
+text = filler.use(template, context)
 
 
-def fill(src, context):
-    first,*parts = src.split("{{")
+Preparse template for repeated use:
+
+tree = filler.parse(template)
+for context in contexts:
+    result.append( filler.execute(tree, context)
+
+
+If you need variable/expression substitution only,
+without control commands, you can use regexp:
+
+import re
+
+def eval_template(template, context):
+    return re.sub(r'\{\{(.*?)\}\}',
+        lambda m: str( eval(m[1], context) ), template)
+
+"""
+
+__version__ = "1.03"
+__all__ = ["fill", "parse", "execute", "use"]
+
+
+def fill(template, context):
+    """Basic templating. Fill vars and expressions."""
+    first,*parts = template.split("{{")
     ans = [first]
     for part in parts:
         cmd,_,text = part.partition("}}")
@@ -16,31 +39,12 @@ def fill(src, context):
     return "".join(ans)
 
 
-def parse(src, open="{%", close="%}"):
-    first,*parts = src.split(open)
-    queued = [dict(cmd="pass", args=None, text=first, children=[])]
-    for part in parts:
-        line,_,text = part.partition(close)
-        cmd,*args = line.split()
-        if cmd.startswith("end"):
-            last = queued.pop()
-            if last['cmd'] != cmd[3:]:
-                raise SyntaxError(f"Template tag {last['cmd']} closed with {cmd}")
-            last['after'] = text
-            queued[-1]['children'].append(last)
-        elif cmd in ("if","for"):
-            queued.append( dict(cmd=cmd, args=args, text=text, children=[]) )
-        else:
-            raise SyntaxError(f"Unknown template tag {cmd}")
-    if len(queued) > 1:
-        raise SyntaxError(f"Tag {queued[-1]['cmd']} is not closed")
-    return queued[0]
-
 
 commands = {}
 
 
 def command(f):
+    """Decorator. Add tag command to registry."""
     tag = f.__name__.removeprefix("cmd_")
     commands[tag] = f
     return f
@@ -48,6 +52,7 @@ def command(f):
 
 @command
 def cmd_pass(tree, context, ans):
+    """Pass (do nothing) tag command."""
     ans.append( fill(tree['text'] , context) )
     for c in tree['children']:
         recurse(c, context, ans)
@@ -75,18 +80,43 @@ def cmd_for(tree, context, ans):
     context.update(outer)
 
 
+def parse(template, open="{%", close="%}"):
+    """Parse template to a tree."""
+    first,*parts = template.split(open)
+    queued = [dict(cmd="pass", args=None, text=first, children=[])]
+    for part in parts:
+        line,_,text = part.partition(close)
+        cmd,*args = line.split()
+        if cmd.startswith("end"):
+            last = queued.pop()
+            if last['cmd'] != cmd[3:]:
+                raise SyntaxError(f"Template tag {last['cmd']} closed with {cmd}")
+            last['after'] = text
+            queued[-1]['children'].append(last)
+        elif cmd in (commands):
+            queued.append( dict(cmd=cmd, args=args, text=text, children=[]) )
+        else:
+            raise SyntaxError(f"Unknown template tag {cmd}")
+    if len(queued) > 1:
+        raise SyntaxError(f"Tag {queued[-1]['cmd']} is not closed")
+    return queued[0]
+
+
 def recurse(tree, context, ans):
+    """Traverse template tree and collect executed pieces"""
     cmd = tree['cmd']
     commands[cmd](tree, context, ans)
     ans.append( fill(tree.get('after',""), context) )
 
 
 def execute(tree, context):
+    """Execute parsed template tree."""
     ans = []
     recurse(tree, context, ans)
     return "".join(ans)
 
 
-def use(src, context):
-    tree = parse(src)
+def use(template, context):
+    """Parse and execute template."""
+    tree = parse(template)
     return execute(tree, context)
